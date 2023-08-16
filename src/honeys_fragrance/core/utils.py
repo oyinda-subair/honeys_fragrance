54915343806b9f927896227b844e63b83f8eca1d
oyinda-subair <oyinda.subair@gmail.com>
2023-08-16 05:57:14 +0100

    set up env variable in settings - load db url based on env

---
+++ src/honeys_fragrance/core/utils.py
@@ -0,0 +1,26 @@
+import os
+import pathlib
+from os.path import join
+from dotenv import load_dotenv
+
+# Project Directories
+ROOT = pathlib.Path(__file__).resolve().parent.parent.parent.parent
+
+dotenv_path = join(ROOT, '.env')
+load_dotenv(dotenv_path)
+
+RUNTIME_ENV = {
+    "dev": "Development",
+    "test": "Test",
+    "prod": "Production"
+}
+
+ENVIRONMENT = os.environ.get("PYTHON_ENV")
+
+
+def get_db_for_by_env():
+    print("Enviroment ===", ENVIRONMENT)
+    if RUNTIME_ENV[ENVIRONMENT] == "Test":
+        return os.environ.get("TEST_DB_URL")
+    else:
+        return os.environ.get("DATABASE_URL")
